135766144c576779676c2b25ca5447bd1511b5d8
mucarb <murilo.rb60@gmail.com>
2022-03-14 14:55:06 -0300

    Testing the first domain class

--- src/main/java/com/ribeiro/murilo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ribeiro/murilo/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.ribeiro.murilo.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ribeiro.murilo.cursomc.domains.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "REST está funcinando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática"); 
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 }
