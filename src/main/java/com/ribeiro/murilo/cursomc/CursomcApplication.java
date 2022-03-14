ff1fd7cf5bf5dd186dee04b777bb3455799572c3
mucarb <murilo.rb60@gmail.com>
2022-03-14 14:16:42 -0300

    Project created

---
+++ src/main/java/com/ribeiro/murilo/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.ribeiro.murilo.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
